430acfd13e7ff8b934a1ed73a3c62cbc285cb54a
Dawid Lebiedzki <lebiedzki@gmail.com>
2021-02-22 21:15:54 +0100

    add constructor and findBy method

--- src/test/java/pages/BasePage.java
+++ src/test/java/pages/BasePage.java
@@ -1,4 +1,19 @@
 package pages;
 
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+import static config.WebDriverSingleton.getInstance;
+import static utillity.Actions.waitForVisibilityOfElement;
+
 public class BasePage {
+
+    public BasePage(){
+        PageFactory.initElements(getInstance(), this);
+        waitForVisibilityOfElement(pageContent);
+    }
+
+    @FindBy(id="columns")
+    private WebElement pageContent;
 }
